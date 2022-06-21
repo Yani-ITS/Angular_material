91fd5186c3b10b565a5de03ff26bad5b44c9ddc3
Yani-ITS <vagnoniyani@gmail.com>
2022-06-21 16:41:58 -0300

    creacion de proyecto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InicioComponent } from './componente/inicio/inicio.component';
+import { ProductosComponent } from './componente/productos/productos.component';
+import { ServiciosComponent } from './componente/servicios/servicios.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:InicioComponent},
+  {path:'productos',component:ProductosComponent},
+  {path:'servicios',component:ServiciosComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,36 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InicioComponent } from './componente/inicio/inicio.component';
+import { ProductosComponent } from './componente/productos/productos.component';
+import { ServiciosComponent } from './componente/servicios/servicios.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MenuprincipalComponent } from './menuprincipal/menuprincipal.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    ProductosComponent,
+    ServiciosComponent,
+    MenuprincipalComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
