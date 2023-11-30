188d3912c9b2d8df6da486de0216a584589a36eb
Yani-ITS <vagnoniyani@gmail.com>
2023-11-30 08:59:10 -0300

    firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,24 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+      apiKey: "AIzaSyCU7aCNCKN1oP-i7-9fFlfAX8S_QMK9-wU",
+
+      authDomain: "app-vagnoni.firebaseapp.com",
+
+      projectId: "app-vagnoni",
+
+      storageBucket: "app-vagnoni.appspot.com",
+
+      messagingSenderId: "1035759304860",
+
+      appId: "1:1035759304860:web:e8fbcbc5027fff741d6d59",
+
+      measurementId: "G-7Z7K52R9RR",
+
+      databaseURL: "https://console.firebase.google.com/u/0/project/app-vagnoni/overview?hl=es",
+  }
 };
 
 /*
